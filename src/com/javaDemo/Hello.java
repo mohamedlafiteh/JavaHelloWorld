64449e929024bc932d89417c4e851b22884e5c4b
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-07-04 12:01:14 +0100

    First java commit

---
+++ src/com/javaDemo/Hello.java
@@ -0,0 +1,10 @@
+package com.javaDemo;
+
+public class Hello {
+
+    public static void printHello() {
+        System.out.println("hello world again!");
+
+    }
+
+}
---
+++ src/com/javaDemo/Main.java
@@ -0,0 +1,9 @@
+package com.javaDemo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world, i am new her!");
+	// write your code here
+    }
+}
